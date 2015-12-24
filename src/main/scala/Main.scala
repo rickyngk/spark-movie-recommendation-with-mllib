613f0e69cfb226710dc45d7e1dc7b1deabb011c6
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-12-24 12:02:09 +0700

    load dataset

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,30 @@
 import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.spark.SparkContext._
 import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.recommendation.{ALS, Rating, MatrixFactorizationModel}
 
 object Main {
+    var sc: SparkContext = null;
     def main(args: Array[String]) {
         val conf = new SparkConf().setAppName("Movie Recommendation")
-		val sc = new SparkContext(conf);
+		sc = new SparkContext(conf)
+        loadRatings("/Users/duynk/projects/movie-recommendation/dataset/ml-1m/ratings.dat")
+        loadMovies("/Users/duynk/projects/movie-recommendation/dataset/ml-1m/movies.dat")
+    }
 
-        println("Hello");
+    def loadRatings(path: String) {
+        val ratings = sc.textFile(path).map { line =>
+            var fields = line.split("::")
+            (fields(3).toLong % 10, Rating(fields(0).toInt, fields(1).toInt, fields(2).toDouble))
+        }
+        println(sc.textFile(path).count)
+    }
+
+    def loadMovies(path: String) {
+        val ratings = sc.textFile(path).map { line =>
+            var fields = line.split("::")
+            (fields(0).toInt, fields(1))
+        }.collect().toMap
+        println(sc.textFile(path).count)
     }
 }
